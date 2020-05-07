fc24e35424f32a72272400f1adfd99425d0f0b8e
v.ITalik <vitalik.gricenko77@gmail.com>
2020-05-07 22:12:00 +0300

    mobile version is ready

--- js/script.js
+++ js/script.js
@@ -6,6 +6,24 @@
     slidesToScroll: 1,
     prevArrow: $('.prev'),
     nextArrow: $('.next'),
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          dots: false
+        }
+      },
+    ]
   });
 
   $('.chefs-slider').slick({
@@ -15,6 +33,26 @@
     arrows: false,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 577,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          dots: false,
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 510,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          dots: false,
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
 
   var $page = $('html, body');
