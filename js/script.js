71a3db01bda7506a5c3cbd9d77ce1d29e8b29dbc
v.ITalik <vitalik.gricenko77@gmail.com>
2020-05-07 15:46:59 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+  $('.about-slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+
+  $('.chefs-slider').slick({
+    infinite: true  ,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1500);
+    return false;
+});
